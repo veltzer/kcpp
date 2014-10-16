5d226561fe9c18b149aabf1d51af85dda666fdce
Mark Veltzer <mark.veltzer@gmail.com>
2014-10-16 10:54:37 +0300

    script change

--- scripts/ubuntu_install.py
+++ scripts/ubuntu_install.py
@@ -3,9 +3,7 @@
 # this script will install all the required packages that you need on
 # ubuntu to compile and work with this package.
 
-# TODO:
-
-import subprocess # for check_call()
+import subprocess # for check_call
 import os # for uname
 
 uname=os.uname()[2]
