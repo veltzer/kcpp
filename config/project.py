2f7b04748b8ab2d9d051429784f24d895324f410
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-18 13:51:01 +0300

    new config

--- config/project.py
+++ config/project.py
@@ -1,4 +1,1 @@
-project_name = "kcpp"
-project_long_description = "C++ framework for the linux kernel"
-project_year_started = 2011
-project_description = "C++ framework for the linux kernel"
+description_short = "C++ framework for the linux kernel"
