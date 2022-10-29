2cacb583e01b607d208bb4279f8871bc9eefbca5
Mark Veltzer <mark.veltzer@gmail.com>
2022-10-29 18:52:00 +0300

    fixed the build for 22.10

--- scripts/process_flags.py
+++ scripts/process_flags.py
@@ -7,6 +7,7 @@ produces the flags for c++ compilation
 
 import sys
 import subprocess
+import re
 
 # General guide to compiler flags (this is true in MOST cases, not in ALL
 # cases, check each flag...)
@@ -56,6 +57,14 @@ def find_ends_with(elems, ending):
     assert found_count==1, f"found too many or too little [{found_count}]"
     return found
 
+def find_first_matches_regexp(elems, regexp):
+    for elem in elems:
+        if regexp.match(elem):
+            return elem
+    assert False, "could not find element by regexp"
+    return None
+
+
 def find_first_ends_with(elems, ending):
     for elem in elems:
         if elem.endswith(ending):
@@ -74,38 +83,42 @@ def main():
 
     args=[
         "/usr/bin/make",
-        "-C","std_module",
+        "-C", "std_module",
         "V=1",
     ]
     if do_pass_kdir:
         args.append(f"KDIR={kdir}")
     if do_clean:
         clean_args=list(args)
-        clean_args.append('clean')
+        clean_args.append("clean")
         output=subprocess.check_output(clean_args)
     output=subprocess.check_output(args, stderr=subprocess.DEVNULL).decode()
-    # split into lines and find the line that ends with 'main.c'
-    lines=output.split('\n')
-    line=find_first_ends_with(lines,'main.c')
-    line=line.strip()
+    # split into lines and find the line that ends with "main.c"
+    lines=output.split("\n")
+    # old code
+    # line=find_first_ends_with(lines, "main.c")
+    # line=line.strip()
+    # new code
+    regexp = re.compile("^ *gcc")
+    line=find_first_matches_regexp(lines, regexp)
+    line = line.split(";")[0].strip()
     flag_list=line.split()
     # remove the last component (which is the source file name, see above)
     flag_list=flag_list[:-1]
-    print(flag_list)
     # header file stuff, does not influence code generation
-    #l=remove(l,'-nostdinc')
+    flag_list = remove_if_exists(flag_list, "-nostdinc")
     # C++ code does not reference any header files of the kernel,
     # operating system, or compiler...
-    # flag_list = remove_begin_with(flag_list,'-I')
+    flag_list = remove_begin_with(flag_list, "-I")
     # remove macros of the kernel (which we dont use in the cpp layer)
-    # flag_list = remove_begin_with(flag_list,'-D')
-    # remove the -Wp,MD
+    # flag_list = remove_begin_with(flag_list, "-D")
+    # remove the -Wp, MD
     # TODO - this was remarked because the -Wp -MD flags were not found. Is this ok?
-    # flag_list=remove_begin_with(flag_list,'-Wp,-MD')
+    flag_list=remove_begin_with(flag_list, "-Wp,-MMD")
     # remove -include and -isystem (which we dont use in the cpp layer)
-    flag_list = remove_two_in_a_row(flag_list, '-isystem')
-    flag_list = remove_two_in_a_row(flag_list, '-include')
-    flag_list = remove_two_in_a_row(flag_list, '-o')
+    flag_list = remove_two_in_a_row(flag_list, "-isystem")
+    flag_list = remove_two_in_a_row(flag_list, "-include")
+    flag_list = remove_two_in_a_row(flag_list, "-o")
     # if the kernel was compiled with debug and profiling then we dont
     # need it
     # flag_list.remove("-pg")
@@ -119,13 +132,18 @@ def main():
     # C/ObjC but not for C++ [enabled by default]
     # cc1plus: warning: command line option "-Wno-pointer-sign" is valid for
     # C/ObjC but not for C++ [enabled by default]
-    flag_list = remove_if_exists(flag_list, "-Wstrict-prototypes")
+    flag_list = remove_if_exists(flag_list, "-Werror=strict-prototypes")
+    flag_list = remove_if_exists(flag_list, "-Werror=implicit-function-declaration")
+    flag_list = remove_if_exists(flag_list, "-Werror=implicit-int")
+    flag_list = remove_if_exists(flag_list, "-Werror=designated-init")
+    flag_list = remove_if_exists(flag_list, "-Werror=incompatible-pointer-types")
     flag_list = remove_if_exists(flag_list, "-Wdeclaration-after-statement")
     flag_list = remove_if_exists(flag_list, "-Wno-pointer-sign")
     flag_list = remove_if_exists(flag_list, "-Wmissing-prototypes")
     flag_list = remove_if_exists(flag_list, "-Wold-style-definition")
     flag_list = remove_if_exists(flag_list, "-std=gnu90")
     flag_list = remove_if_exists(flag_list, "-std=gnu89")
+    flag_list = remove_if_exists(flag_list, "-std=gnu11")
     with open(outfile, "w", encoding="UTF8") as stream:
         stream.write(" ".join(flag_list))
 
