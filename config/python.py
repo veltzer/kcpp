2fc743b4df3998150b55d61e01b0b9963d229797
Mark Veltzer <mark.veltzer@gmail.com>
2023-12-29 19:17:51 +0200

    fix for new pydmt

--- config/python.py
+++ config/python.py
@@ -1,8 +1,11 @@
-""" python dependencies for this project """
-
+config_requires = []
+dev_requires = []
+install_requires = []
 make_requires = [
     "pymakehelper",
     "pydmt",
     "pyclassifiers",
     "pylint",
 ]
+test_requires = []
+requires = config_requires + install_requires + make_requires + test_requires
