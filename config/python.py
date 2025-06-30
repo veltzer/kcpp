35468290252ae1ed14756083eae28c8020a0c262
Mark Veltzer <mark.veltzer@gmail.com>
2025-06-30 14:09:43 +0300

    fixed up deps

--- config/python.py
+++ config/python.py
@@ -1,9 +1,7 @@
 """ python deps for this project """
 
-build_requires: list[str] = [
-    "pyclassifiers",
-    "pydmt",
-    "pymakehelper",
-    "pylint",
-]
-requires = build_requires
+import config.shared
+
+build_requires: list[str] = config.shared.BUILD
+test_requires: list[str] = config.shared.TEST
+requires = build_requires + test_requires
